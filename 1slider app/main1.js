1c39f7550a895c1f4f4c9228d47ec31ee948937d
Baturpcu <batusalkim11@gmail.com>
2022-10-22 19:36:33 +0300

    javascript projects
    Projects I developed while learning javascript

---
+++ 1slider app/main1.js
@@ -0,0 +1,63 @@
+var models = [
+    {
+        name: 'haryy',
+        image: 'images/foto1.jpg',
+        link: 'https://www.google.com/search?q=goole&oq=goole&aqs=chrome..69i57j46i10i131i199i433i465j0i10i131i433l3j0i10i433j0i10i131i433l3j69i59.5297j0j7&sourceid=chrome&ie=UTF-8'
+    },
+    {
+        name: 'kjashdjkhsjdhkas',
+        image: 'images/foto2.jpg',
+        link: 'https://www.google.com/search?q=goole&oq=goole&aqs=chrome..69i57j46i10i131i199i433i465j0i10i131i433l3j0i10i433j0i10i131i433l3j69i59.5297j0j7&sourceid=chrome&ie=UTF-8'
+    },
+    {
+        name: 'çsjdkllsadjklsdj',
+        image: 'images/foto3.jpg',
+        link: 'https://www.facebook.com/checkpoint/1501092823525282/?next=https%3A%2F%2Fwww.facebook.com%2F'
+    },
+    {
+        name: 'asdsadasdsa',
+        image: 'images/foto4.jpg',
+        link: 'https://www.google.com/search?q=goole&oq=goole&aqs=chrome..69i57j46i10i131i199i433i465j0i10i131i433l3j0i10i433j0i10i131i433l3j69i59.5297j0j7&sourceid=chrome&ie=UTF-8'
+    },
+    {
+        name: 'asdsadasdsa',
+        image: 'images/foto5.jpg',
+        link: 'https://www.google.com/search?q=goole&oq=goole&aqs=chrome..69i57j46i10i131i199i433i465j0i10i131i433l3j0i10i433j0i10i131i433l3j69i59.5297j0j7&sourceid=chrome&ie=UTF-8'
+    },
+];
+
+var index = 0;
+
+var resimSayisi = models.length;
+
+document.querySelector(".fa-arrow-circle-left").addEventListener("click",()=>{
+    index--
+    console.log(index);
+})
+
+document.querySelector(".fa-arrow-circle-right").addEventListener("click",()=>{
+    index++;
+    console.log(index);
+})
+
+function ResimGöster(i){
+    index = i;
+
+    if(i<0){
+        index=resimSayisi-1;
+    }
+    if(i>resimSayisi){
+        index=0;
+    }
+
+    document.querySelector(".card-title").textContent=models[index].name;
+    document.querySelector(".card-img-top").setAttribute("src",models[index].image);
+
+    document.querySelector(".card-link").setAttribute("src",models[index].link);
+
+}
+
+
+
+
+
