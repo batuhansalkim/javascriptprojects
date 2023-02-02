e6188650c6280840a4c59bc38b3b60dcea916096
Baturpcu <batusalkim11@gmail.com>
2023-02-03 02:44:37 +0300

    last update

--- js projects/diger/6/hex.js
+++ js projects/diger/6/hex.js
@@ -23,36 +23,52 @@ var models = [
 ]
 var index =0;
 var slaytCount = models.length;
-
 showSlide(index);
 
+
+showSlide(index);
+console.log(index);
+document.querySelector(".fa-arrow-circle-right").addEventListener("click",function(){
+    index++;
+    
+    showSlide(index);
+});
 document.querySelector(".fa-arrow-circle-left").addEventListener("click",function(){
     index--;
     showSlide(index);
-    
-});
-
-document.querySelector(".fa-arrow-circle-right").addEventListener("click",function(){
-    index++;
-    showSlide(index);
-    
-});
-
+})
 function showSlide(i){
     index = i;
     if(i<0){
         index = slaytCount -1;
     }
-    if(i >= slaytCount){
-        index = 0;
+    if(i>=slaytCount){
+        index =0;
     }
-
-   document.querySelector(".card-title").textContent = models[index].name;
-
-    document.querySelector(".card-img-top").setAttribute('src',models[index].image)
-
-    document.querySelector(".card-link").setAttribute("href",models[index].link); 
+    document.querySelector(".card-title").textContent = models[index].name;
+    document.querySelector(".card-img-top").setAttribute("src",models[index].image);
+    document.querySelector(".card-link").setAttribute("href",models[index].link);
 }
+
+// var index =0;
+// var slaytCount = models.length;
+// showSlide(index);
+
+// function showSlide(i){
+//     index = i;
+//     if(i<0){
+//         index = slaytCount -1;
+//     }
+//     if(i >= slaytCount){
+//         index = 0;
+//     }
+
+//    document.querySelector(".card-title").textContent = models[index].name;
+
+//     document.querySelector(".card-img-top").setAttribute('src',models[index].image)
+
+//     document.querySelector(".card-link").setAttribute("href",models[index].link); 
+// }
 
 
 
