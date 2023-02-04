1d12e84a6351069c710a33e7f63f14695ec0deb4
Baturpcu <batusalkim11@gmail.com>
2023-02-04 05:26:17 +0300

    last update

--- js projects/03Cinema ticket reservation/main.js
+++ js projects/03Cinema ticket reservation/main.js
@@ -1,43 +1,63 @@
-const container = document.querySelector(".container") ;
-const count = document.getElementById("count") ;
+// const container = document.querySelector(".container") ;
+// const count = document.getElementById("count") ;
+// const amount = document.getElementById("amount");
+// const select = document.getElementById("movie");
+// const seats = document.querySelectorAll(".seat:not(.reserved)")
+
+
+// container.addEventListener("click",function(e){
+//     if(e.target.classList.contains("seat") && e.target.classList.contains("reserved")){
+//         e.target.classList.toggle("selected");
+//     }
+// })
+
+
+// container.addEventListener("click", function(e){
+//     if(e.target.classList.contains("seat") && !e.target.classList.contains("reserved")){
+//         e.target.classList.toggle("selected");
+
+//         calculateTotal();
+
+//     }
+// });
+
+// select.addEventListener("change",function(e){
+//     calculateTotal();
+// })
+
+// function calculateTotal(){
+//     const selectedSeats = container.querySelectorAll(".seat.selected");
+
+//     console.log(seats);
+//     console.log(selectedSeats);
+
+//     let selectedSeatCount = selectedSeats.length; 
+//     count.innerText=selectedSeatCount;
+//     amount.innerText= selectedSeatCount * select.value;;
+// }
+
+const container = document.querySelector(".container");
+const count = document.getElementById("count");
 const amount = document.getElementById("amount");
 const select = document.getElementById("movie");
-const seats = document.querySelectorAll(".seat:not(.reserved)")
-
 
 container.addEventListener("click",function(e){
-    if(e.target.classList.contains("seat") && e.target.classList.contains("reserved")){
+    if(e.target.classList.contains("seat")&& !e.target.classList.contains("reserved")){
         e.target.classList.toggle("selected");
+
+        ToplamFiyatHesapla();
     }
 })
-
-
-container.addEventListener("click", function(e){
-    if(e.target.classList.contains("seat") && !e.target.classList.contains("reserved")){
-        e.target.classList.toggle("selected");
-
-        calculateTotal();
-
-    }
+select.addEventListener("change",function(e){
+    ToplamFiyatHesapla();
 });
 
-select.addEventListener("change",function(e){
-    calculateTotal();
-})
-
-function calculateTotal(){
-    const selectedSeats = container.querySelectorAll(".seat.selected");
-
-    console.log(seats);
-    console.log(selectedSeats);
-
-    let selectedSeatCount = selectedSeats.length; 
-    count.innerText=selectedSeatCount;
-    amount.innerText= selectedSeatCount * select.value;;
+function ToplamFiyatHesapla(){
+    let secilikoltuksayisi = container.querySelectorAll(".seat.selected").length;
+    let price = select.value;
+    count.innerText = secilikoltuksayisi;
+    amount.innerText = secilikoltuksayisi * price;
 }
-
-
-
 
 
 
