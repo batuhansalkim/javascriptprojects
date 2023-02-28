1065472d295efe8d353f96452b640045313fa444
Baturpcu <batusalkim11@gmail.com>
2023-02-28 18:13:50 +0300

    update

---
+++ js projects/30Dk30FarklıJsMetod/hex.js
@@ -0,0 +1,26 @@
+const arr = [1,2,3,4,5,6,7]
+const arr1 = [8,9,10]
+
+
+console.log(arr);
+console.log(arr[1]);
+arr.push(8);
+console.log(arr);
+arr.shift();
+console.log(arr);
+arr.concat(arr1);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
