4a0cf6929cee02c0149f18e43f74a236620b1aa2
Baturpcu <batusalkim11@gmail.com>
2023-03-01 17:26:22 +0300

    update

--- js projects/30Dk30FarklıJsMetod/ders2/hex.js
+++ js projects/30Dk30FarklıJsMetod/ders2/hex.js
@@ -1,3 +1,4 @@
+//SORU 1
 { 
 //SORU 1
 //[8,4,6,2,3,] => output => [4,2,4,2,3] --> birbirinden cıkararak ilerleme metotları
@@ -17,17 +18,34 @@
 
 // console.log(solution([8, 4, 6, 2, 3,]));
 }
-
 //SORU 2
+{
+    //SORU 2
+    //input [[4,3,2,-1], [3,-2,-1,6],[5,-4,0,-1]] kac - li deger var => 5 
 
-//input [[4,3,2,-1], [3,-2,-1,6],[5,-4,0,-1]] kac - li deger var => 5 
 
+    // const solution = (arr) => {
+    //     let newArr = [];
+    //     for (let i = 0; i < arr.length; i++) {
+    //         for (let j = 0; j < arr[i].length; j++) {
+    //             if (arr[i][j] < 0) {
+    //                 newArr.push(arr[i][j]);
+    //             }
+    //         }
 
-const solution = (arr)=>{
-    return arr.map(row=> row.filter(r=> r<0).length).reduce((a,b)=> a+b, 0);
+    //     }
+    //     return newArr.length;
+    // }
+    // console.log(solution([[4, 3, 2, -1], [3, -2, -1, 6], [5, -4, 0, -1]]))
+
 }
 
-console.log(solution([[4, 3, 2, -1], [3, -2, -1, 6], [5, -4, 0, -1]]))
+//SORU 3
+{
+    const solution = ()=>{
+        
+    }
+}
 
 
 
