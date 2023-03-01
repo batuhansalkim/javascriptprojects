2051666cd0dc84280235f9679d0e20b99b859dc1
Baturpcu <batusalkim11@gmail.com>
2023-03-01 16:25:47 +0300

    update

--- js projects/30Dk30FarklıJsMetod/ders2/hex.js
+++ js projects/30Dk30FarklıJsMetod/ders2/hex.js
@@ -1,1 +1,77 @@
-console.log("batu")
+//[8,4,6,2,3,] => output => [4,2,4,2,3] --> birbirinden cıkararak ilerleme metotları
+
+// const solution = (arr) =>{
+//     return arr.map((_,i)=>{
+//         for(let j = i+1; j<arr.length; j++){
+//             if(arr[i] >arr[j]){
+//                 arr[i] = arr[i] - arr[j];
+//                 break;
+//             }
+//         }
+//         return arr[i];
+//     })
+// }
+
+const solution = (arr)=>{
+    return arr.map((_,i)=>{
+        for(let j = i+1; j<arr.length;j++){
+            if(arr[i]> arr[j]){
+                arr[i] = arr[i] - arr[j];
+                break;
+            }
+        }
+        return arr[i];
+    })
+}
+
+console.log(solution([8, 4, 6, 2, 3,]));
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
