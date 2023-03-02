5456c14af6982c92426694f45209d565f78afe9e
Baturpcu <batusalkim11@gmail.com>
2023-03-03 02:17:10 +0300

    last update

--- js projects/1slider app/main.js
+++ js projects/1slider app/main.js
@@ -1,38 +1,54 @@
 var models = [
     {
-        name: 'haryy',
+        name: 'resim 1',
         image: 'images/foto1.jpg',
         link: 'https://www.google.com/search?q=goole&oq=goole&aqs=chrome..69i57j46i10i131i199i433i465j0i10i131i433l3j0i10i433j0i10i131i433l3j69i59.5297j0j7&sourceid=chrome&ie=UTF-8'
     },
     {
-        name: 'kjashdjkhsjdhkas',
+        name: 'resim 2',
         image: 'images/foto2.jpg',
         link: 'https://www.google.com/search?q=goole&oq=goole&aqs=chrome..69i57j46i10i131i199i433i465j0i10i131i433l3j0i10i433j0i10i131i433l3j69i59.5297j0j7&sourceid=chrome&ie=UTF-8'
     },
     {
-        name: 'çsjdkllsadjklsdj',
+        name: 'resim 3',
         image: 'images/foto3.jpg',
         link: 'https://www.facebook.com/checkpoint/1501092823525282/?next=https%3A%2F%2Fwww.facebook.com%2F'
     },
     {
-        name: 'asdsadasdsa',
+        name: 'resim 4',
         image: 'images/foto4.jpg',
         link: 'https://www.google.com/search?q=goole&oq=goole&aqs=chrome..69i57j46i10i131i199i433i465j0i10i131i433l3j0i10i433j0i10i131i433l3j69i59.5297j0j7&sourceid=chrome&ie=UTF-8'
     },
     {
-        name: 'asdsadasdsa',
+        name: 'resim 5',
         image: 'images/foto5.jpg',
         link: 'https://www.google.com/search?q=goole&oq=goole&aqs=chrome..69i57j46i10i131i199i433i465j0i10i131i433l3j0i10i433j0i10i131i433l3j69i59.5297j0j7&sourceid=chrome&ie=UTF-8'
     },
 ]
 
-var index = 1;
+var index = 0;
+
+document.querySelector(".fa-arrow-circle-left").addEventListener("click",function(){
+    index++;
+    if(index>models.length){
+        alert("fazla deger giriliyor...")
+    }
+    console.log(index);
+});
+
+
+document.querySelector(".fa-arrow-circle-right").addEventListener("click",function(){
+
+});
+
+
+
 
 document.querySelector(".card-title").textContent = models[index].name;
 
 document.querySelector(".card-img-top").setAttribute("src",models[index].image);
 
-
+document.querySelector(".card-link").setAttribute("href",models[index].link);
 
 // var index = 0;
 // var resimSayisi = models.length;
