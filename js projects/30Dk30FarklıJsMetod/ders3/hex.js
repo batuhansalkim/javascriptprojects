e97d1678b786c66b7a98f66395e1bce21067ff50
Baturpcu <batusalkim11@gmail.com>
2023-03-04 04:49:28 +0300

    update

---
+++ js projects/30Dk30FarklıJsMetod/ders3/hex.js
@@ -0,0 +1,12 @@
+function whosPaying() {
+    var list = ["albardak","batu","cörüt","ulas","neco","baris",];
+    var lenofList = list.length;
+    var randomNumber = Math.floor(Math.random() * (lenofList));
+
+    return list[randomNumber] + "hesabi sen ödüyon kanks geçmiş olsun ajshdjhsajdgjsagdjga";
+
+}
+
+
+
+
